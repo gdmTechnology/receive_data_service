d08ea3cfdc9ba780214c3b6cd702c5464a9175ae
guiac <gui.acassemiro@gmail.com>
2023-01-31 20:31:56 -0300

    :wrench: setup env

--- src/main/config/kafka/kafka.server.ts
+++ src/main/config/kafka/kafka.server.ts
@@ -4,8 +4,8 @@ import { KafkaProducer } from '@/infra/kafka'
 
 export const setupKafka = async (): Promise<KafkaProducer> => {
     const kafkaServer = new Kafka({
-        clientId: env.KAFKA_CLIENTID,
-        brokers: [`${env.KAFKA_BROKER_HOST}: ${env.KAFKA_BROKER_PORT}`]
+        clientId: env.kafkaClientId,
+        brokers: [`${env.kafkaBrokerHost}: ${env.kafkaBrokerPort}`]
     })
     const kafkaProducer = new KafkaProducer(kafkaServer)
     await kafkaProducer.kafkaProducer()
