db396ab7690bf4a5b09e1411eb4bf629e1535736
guiac <gui.acassemiro@gmail.com>
2023-01-30 23:17:15 -0300

    :sparkles: setup docker

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,7 @@
 export default {
-  port: process.env.PORT || 3002,
+  port: process.env.PORT || 3004,
   jwtSecret: process.env.JWT_SECRET || '1kZDnw8==jh',
-  mongoUrl: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/myapp',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/rem',
   KAFKA_CLIENTID: process.env.KAFKA_CLIENTID || 'rem-kafka',
   KAFKA_BROKER_PORT: process.env.KAFKA_BROKER_PORT || 9092,
   KAFKA_BROKER_HOST: process.env.KAFKA_BROKER_HOST || 'broker'
