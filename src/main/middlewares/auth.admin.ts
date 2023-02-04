268f308f06d37f3f6826ee7b9d7651cdb77cc932
guiac <gui.acassemiro@gmail.com>
2023-02-04 12:10:45 -0300

    :construction: removing unused files

--- src/main/middlewares/auth.admin.ts
+++
@@ -1,4 +0,0 @@
-import { makeAuthMiddleware } from '@/main/factories'
-import { adaptMiddleware } from '@/main/adapters'
-
-export const authAdmin = adaptMiddleware(makeAuthMiddleware('admin'))
--- src/main/middlewares/auth.ts
+++
@@ -1,4 +0,0 @@
-import { makeAuthMiddleware } from '@/main/factories'
-import { adaptMiddleware } from '@/main/adapters'
-
-export const auth = adaptMiddleware(makeAuthMiddleware())
